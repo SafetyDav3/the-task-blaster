8e659a0a88f7df452c6288c7045d47869abd9e27
SafteyDav3 <talktomedw@gmail.com>
2022-03-24 02:59:01 -0500

    Beaut the file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function () {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
