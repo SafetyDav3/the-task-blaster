1cf7b795d2b6aa414a99bd48da9c5d5adbd88f4e
SafteyDav3 <talktomedw@gmail.com>
2022-03-24 23:46:12 -0500

    Left off at task name input

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']");
+  console.log(taskNameInput);
 
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
