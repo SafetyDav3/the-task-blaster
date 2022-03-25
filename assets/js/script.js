631d73d31be13b42917af89787f2554cd1550d80
SafteyDav3 <talktomedw@gmail.com>
2022-03-24 23:37:19 -0500

    Form field values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
