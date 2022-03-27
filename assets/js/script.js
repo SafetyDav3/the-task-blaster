abd7f27a0fbaa092d76af5b966e2a4550496e841
SafteyDav3 <talktomedw@gmail.com>
2022-03-26 20:26:11 -0500

    Form reset and if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,13 @@ var taskFormHandler = function (event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  if (!taskNameInput || !taskTypeInput) {
+    alert("Please enter a task name and type");
+    return false;
+  }
+
+  formEl.reset();
+
   // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
